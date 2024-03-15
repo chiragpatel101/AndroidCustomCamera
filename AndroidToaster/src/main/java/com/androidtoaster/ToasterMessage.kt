0aa14ac66a9e2bf2a3aa3011d3e4be9b3264830e
Chirag Patel <chirag.patel@clariontechnologies.co.in>
2024-03-15 12:18:29 +0530

    update build

--- AndroidToaster/src/main/java/com/androidtoaster/ToasterMessage.kt
+++ AndroidToaster/src/main/java/com/androidtoaster/ToasterMessage.kt
@@ -3,8 +3,8 @@ package com.androidtoaster
 import android.content.Context
 import android.widget.Toast
 
-public class ToasterMessage {
-    public fun showToast(context : Context, message : String){
+object ToasterMessage {
+     fun showToast(context : Context, message : String){
         Toast.makeText(context,message,Toast.LENGTH_LONG).show()
     }
 }
