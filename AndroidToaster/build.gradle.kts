44695c385e2a4e0a1525e465a9f3bc781ce17531
Chirag Patel <chirag.patel@clariontechnologies.co.in>
2024-03-15 17:54:49 +0530

    update build

--- AndroidToaster/build.gradle.kts
+++ AndroidToaster/build.gradle.kts
@@ -1,7 +1,17 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish") // Apply plugin
+
 }
+// Declare library details
+val mGroupId = "com.androidtoaster"
+val mArtifactId = "toastermessage"
+val mVersionCode = 8
+val mVersionName = "1.0.9"
+
+val mLibraryName = "toastermessage"
+val mLibraryDescription = "Simple Library for simple things!"
 
 android {
     namespace = "com.androidtoaster"
@@ -41,3 +51,44 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+// Declare task for creation of android sources.
+val androidSourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(android.sourceSets["main"].java.srcDirs)
+}
+
+// Make configuration for publishing artifact.
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = mGroupId
+                artifactId = mArtifactId
+                version = mVersionName
+
+                from(components["release"])
+
+                artifact(androidSourcesJar)
+
+                pom {
+                    name.set(mLibraryName)
+                    description.set(mLibraryDescription)
+                }
+            }
+        }
+
+        // Update repository details and credentials.
+//        repositories {
+//            maven {
+//                name = "GitHubPackages"
+//                url = uri("https://github.com/chiragpatel101/AndroidToaster")
+//                credentials {
+//                    username = System.getenv("GPR_USER")
+//                    password = System.getenv("GPR_KEY")
+//                }
+//            }
+//        }
+    }
+}
+
+// Assembling should be performed before publishing package
