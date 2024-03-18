b3cff3781fdde18681502aff22fd7f36805ffdce
Chirag Patel <chirag.patel@clariontechnologies.co.in>
2024-03-18 13:55:17 +0530

    update build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,9 +49,12 @@ android {
             )
         }
     }
-
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
